5002299a567a1fdcccd179173548ba1ccb270628
thienkk25 <thiennguyendz25@gmail.com>
2025-01-16 21:35:26 +0700

    add screen login

---
+++ lib/screens/login.dart
@@ -0,0 +1,126 @@
+import 'package:app_chat/screens/forgot.dart';
+import 'package:app_chat/screens/register.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 4,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                "Login",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 1.5,
+            child: Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusColor: Colors.black,
+                      labelText: "Email",
+                      labelStyle: const TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                  cursorColor: Colors.black,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: const Icon(Icons.visibility),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusColor: Colors.black,
+                      labelText: "Password",
+                      labelStyle: const TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                  cursorColor: Colors.black,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (_) => const Forgot()));
+                    },
+                    child: const Text(
+                      "Forgot",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 3,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    )),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("You don't have account?"),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const Register()));
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
